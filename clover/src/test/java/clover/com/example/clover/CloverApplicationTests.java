e38c565a8db517efa2f4939bef063c1ece664a24
jack <singhv248@outlook.com>
2024-03-03 06:47:40 +0000

    adding clover project

---
+++ clover/src/test/java/clover/com/example/clover/CloverApplicationTests.java
@@ -0,0 +1,13 @@
+package clover.com.example.clover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
